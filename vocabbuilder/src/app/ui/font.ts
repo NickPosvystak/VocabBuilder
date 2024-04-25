32e343013b1f3411901b3333d47eddeec4c3b776
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-04-25 12:41:26 +0200

    fonts

---
+++ vocabbuilder/src/app/ui/font.ts
@@ -0,0 +1,7 @@
+//  primary font
+import { Inter } from "next/font/google";
+export const inter = Inter({ subsets: ["latin"] });
+
+//  secondary font
+import { Lusitana } from "next/font/google";
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ["400", "700" ]});
